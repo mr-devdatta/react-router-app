2e96497c41356b25454c742fff3c2282aa14d0cb
Devdatta <mr.devdatta@gmail.com>
2025-10-27 00:43:06 +0530

    Adding react router menu style

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/styles/index.css";
+import MainPage from "./pages/MainPage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <div className="my-border bg-gray-100 p-1">
+            <MainPage />
+        </div>
+    </StrictMode>
+);
